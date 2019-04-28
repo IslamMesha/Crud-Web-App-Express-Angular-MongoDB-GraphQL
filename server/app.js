93963fdb17af2dd921468e372060d42745692d67
Islam Mesha <eng.islammesha@gmail.com>
2019-04-28 15:10:14 +0200

    Finish the server

---
+++ server/app.js
@@ -0,0 +1,59 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var graphqlHTTP = require('express-graphql');
+var cors = require('cors');
+var bookSchema = require('./graphql/bookSchema');
+
+// Connect to Database
+mongoose.connect('mongodb://localhost/graphql', { promiseLibrary: require('bluebird'), useNewUrlParser: true }).then(() => {
+  console.log('Database connected successfully!!');
+}).catch((err) => {
+  console.error('Failed to connect: ', err);
+});
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// Add Middlewares
+app.use(logger('dev'));
+app.use('*', cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/graphql', cors(), graphqlHTTP({
+  schema: bookSchema,
+  rootValue: global,
+  graphiql: true
+}));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
