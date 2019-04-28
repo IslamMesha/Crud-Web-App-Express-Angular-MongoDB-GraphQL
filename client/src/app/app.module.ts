e83d1511fcfa1a4d9065666be2b250f056978b04
Islam Mesha <eng.islammesha@gmail.com>
2019-04-28 16:31:03 +0200

    Generate book components.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,17 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { BooksComponent } from './components/books/books.component';
+import { DetailComponent } from './components/books/detail/detail.component';
+import { EditComponent } from './components/books/edit/edit.component';
+import { AddComponent } from './components/books/add/add.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    DetailComponent,
+    EditComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    apollo: Apollo,
+    httpLink: HttpLink
+  ) {
+    apollo.create({
+      link: httpLink.create({ uri: 'http://localhost:3000/graphql' }),
+      cache: new InMemoryCache()
+    });
+  }
+}
